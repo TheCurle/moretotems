1bb2ce867dad0eeab58bfcfe4776b2bcdadc08e9
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-07-18 16:41:33 -0400

    Clean up

--- src/main/java/samebutdifferent/mobtotems/MobTotems.java
+++ src/main/java/samebutdifferent/mobtotems/MobTotems.java
@@ -6,8 +6,6 @@ import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -28,19 +26,8 @@ public class MobTotems {
 
     public MobTotems() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
         ModItems.ITEMS.register(bus);
         MinecraftForge.EVENT_BUS.register(new ModEvents());
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
     }
 }
