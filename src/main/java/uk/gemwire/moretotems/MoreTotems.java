6edf46a6314ba6d8d96d21f90e36d942951eaec1
Curle <curle@gemwire.uk>
2021-07-24 18:45:15 +0100

    Port to 1.17

--- src/main/java/uk/gemwire/moretotems/MoreTotems.java
+++ src/main/java/uk/gemwire/moretotems/MoreTotems.java
@@ -1,14 +1,14 @@
 package uk.gemwire.moretotems;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
@@ -30,7 +30,7 @@ public class MoreTotems {
 
     public static final RegistryObject<Item> TOTEM_OF_ESCAPING = ITEMS.register("totem_of_escaping", TotemItem::new);
 
-    public static final ItemGroup GROUP = new ItemGroup(MoreTotems.MOD_ID) {
+    public static final CreativeModeTab GROUP = new CreativeModeTab(MoreTotems.MOD_ID) {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.TOTEM_OF_UNDYING);
--- src/main/java/uk/gemwire/moretotems/event/ModEvents.java
+++ src/main/java/uk/gemwire/moretotems/event/ModEvents.java
@@ -1,17 +1,17 @@
 package uk.gemwire.moretotems.event;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.monster.EndermanEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.World;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,19 +22,20 @@ public class ModEvents {
     @SubscribeEvent
     public void useTotem(LivingDeathEvent event) {
         LivingEntity entity = event.getEntityLiving();
-        World world = entity.level;
-        if (entity instanceof PlayerEntity) {
-            for (Hand hand : Hand.values()) {
+        Level world = entity.level;
+        if (entity instanceof Player) {
+            for (InteractionHand hand : InteractionHand.values()) {
                 ItemStack heldItem = entity.getItemInHand(hand);
+
                 if (heldItem.getItem() == MoreTotems.TOTEM_OF_ESCAPING.get()) {
                     TotemItem.defaultTotemBehavior(event, entity, heldItem, MoreTotems.TOTEM_OF_ESCAPING.get());
-                    entity.addEffect(new EffectInstance(Effects.INVISIBILITY, 200, 0));
+                    entity.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 200, 0));
                     for(int i = 0; i < 16; ++i) {
                         double xRand = entity.getX() + (entity.getRandom().nextDouble() - 0.5D) * 16.0D;
-                        double yRand = MathHelper.clamp(entity.getY() + (double) (entity.getRandom().nextInt(16) - 8), 0.0D, (double) (world.getHeight() - 1));
+                        double yRand = Mth.clamp(entity.getY() + (double) (entity.getRandom().nextInt(16) - 8), 0.0D, (double) (world.getHeight() - 1));
                         double zRand = entity.getZ() + (entity.getRandom().nextDouble() - 0.5D) * 16.0D;
                         entity.randomTeleport(xRand, yRand, zRand, true);
-                        world.playSound((PlayerEntity) null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        world.playSound((Player) null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
                         entity.playSound(SoundEvents.CHORUS_FRUIT_TELEPORT, 1.0F, 1.0F);
                     }
                 }
@@ -42,12 +43,13 @@ public class ModEvents {
         }
     }
 
+    // TODO: Replace with a GLM
     @SubscribeEvent
     public void mobDropTotem(LivingDropsEvent event) {
         LivingEntity entity = event.getEntityLiving();
-        World world = entity.getCommandSenderWorld();
+        Level world = entity.getCommandSenderWorld();
         if (Math.random() <= 0.05) {
-            if (entity instanceof EndermanEntity)
+            if (entity instanceof EnderMan)
                 event.getDrops().add(new ItemEntity(world, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(MoreTotems.TOTEM_OF_ESCAPING.get())));
         }
     }
